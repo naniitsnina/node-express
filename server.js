77fb6c97c3448a96547ee7f4394846cbe1ebded4
Nirel Chua <95299412+cninz@users.noreply.github.com>
2023-09-14 13:14:34 -0600

    Intro to Express Part 2

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const hostname = 'localhost'
+const morgan = require('morgan')
 const port = 3000
 const app = express()
+
+app.use(morgan('dev'))
+
+app.use(express.static(__dirname + '/public'))
 
 app.use((req, res) => {
     console.log(req.headers)
